ced305cee416fba4f626eeab3dc2ff289fa7c4a8
soulayman <soulaymanjaa09@gmail.com>
2025-09-17 14:20:16 +0100

    changing page to gameplay page with dynamic grid boxes positioning

--- js/script.js
+++ js/script.js
@@ -1,13 +1,58 @@
 
 
-function Play(){
+function Play() {
     let GridNumber = document.getElementById('GridNumber').value;
-    console.log(GridNumber);
 
-    document.body.innerHTML=`
-    
-    div
+   let body = document.getElementById('main');
+body.innerHTML=`
+<div class="gamePage" >
 
-    `
+   <div id="grid">
+     <div id="gridBoxes">
+
+
+       <div id="boxes">
+       <p>hiiiiiii</p>
+       </div>
+
+     </div>
+    </div>
+</div>
+`
+
+setUpGrid(GridNumber)
 
 }
+
+
+
+
+function setUpGrid(GN){
+
+    
+    let boxes=  document.getElementById('boxes');
+    
+    boxes.innerHTML="";
+    
+      boxes.style.gridTemplateColumns = `repeat(${GN}, 1fr)`;
+  boxes.style.gridTemplateRows = `repeat(${GN}, 1fr)`;
+    
+    for (let i = 0; i < GN*GN; i++) {
+
+        boxes.innerHTML+=`    
+ <div  id="box" onclick=pressBox()>
+       <p>i </p>
+       </div>
+        `    
+        console.log(boxes);
+
+
+    }
+   
+
+}
+
+
+       
+
+
