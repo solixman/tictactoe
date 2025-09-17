7f8dd6bd1d4cee68322bf0d1c1cc7d6e636935a8
soulayman <soulaymanjaa09@gmail.com>
2025-09-17 20:26:06 +0100

    storing data in local storage

--- js/script.js
+++ js/script.js
@@ -2,103 +2,116 @@
 
 function Play() {
     let GridNumber = document.getElementById('GridNumber').value;
+    localStorage.setItem("GridNumber",GridNumber);
 
+    let k = document.getElementById('k').value;
+    localStorage.setItem("k",k);
+
+    
     let body = document.getElementById('main');
     body.innerHTML = `
 <div class="gamePage" >
 
-   <div id="grid">
-     <div id="gridBoxes">
+<div id="grid">
+<div id="gridBoxes">
 
-     <h3 class='gameStatus'></h3>
+<h3 class='gameStatus'></h3>
 
 
-       <div id="boxes">
-       <p>hiiiiiii</p>
+<div id="boxes">
+<p>hiiiiiii</p>
        </div>
 
-     </div>
-    </div>
-    
-    <div class="ActionButtons">
-
-        <button id="resetButton" type='button' onclick=Play() button>Reset game</button>
-
-        <button id="StartNewGameButton" type='button' onclick=Play() button>Start New Game </button>
-
-    </div>
-
-</div>
-
-`
-
-    setUpGrid(GridNumber)
-
-}
-
-
-
-
-function setUpGrid(GN) {
-
-
-    let boxes = document.getElementById('boxes');
-
-    boxes.innerHTML = "";
-
-    boxes.style.gridTemplateColumns = `repeat(${GN}, 1fr)`;
-    boxes.style.gridTemplateRows = `repeat(${GN}, 1fr)`;
-
-    for (let i = 0; i < GN * GN; i++) {
-
-        boxes.innerHTML += `    
- <div data-cell-index = ${i}  id="box" onclick="pressBox(this)">
-      
        </div>
+       </div>
+       
+       <div class="ActionButtons">
+       
+       <button id="resetButton" type='button' onclick=Play() button>Reset game</button>
+       
+       <button id="StartNewGameButton" type='button' onclick=Play() button>Start New Game </button>
+       
+       </div>
+       
+       </div>
+       
+       `
+       
+       setUpGrid(GridNumber)
+       
+    }
     
-
+    
+    
+    
+    function setUpGrid(GN) {
+        
+        
+        let boxes = document.getElementById('boxes');
+        
+        boxes.innerHTML = "";
+        
+        boxes.style.gridTemplateColumns = `repeat(${GN}, 1fr)`;
+        boxes.style.gridTemplateRows = `repeat(${GN}, 1fr)`;
+        
+        for (let i = 0; i < GN * GN; i++) {
+            
+            boxes.innerHTML += `    
+            <div data-cell-index = ${i}  id="box" onclick="pressBox(this)">
+            
+            </div>
+            
+            
         `
-
+        
 
 
     }
     
-
+    
 }
 
 
 
 let currentPlayer="X";
 let gameActive=true;
+localStorage.setItem("scoreX",0);
+localStorage.setItem("scoreO",0);
+
 
 function pressBox(box) {
-       i=box.getAttribute("data-cell-index");
+    i=box.getAttribute("data-cell-index");
     box.innerHTML="";
-
-    if(!gameActive){
-return;
-    }
-
-    if(currentPlayer === "X"){
-
     
+    if(!gameActive){
+        return;
+    }
+    
+    if(currentPlayer === "X"){
+        
+        
         box.innerHTML=`
         <div data-cell-index = ${i}  id="box" onclick="pressBox(this)">
         <img src="/src/icons/X.png" alt="X" style="width:95%; height:95%;">
         </div>
         `
         currentPlayer="O";
-     }
-     else if (currentPlayer === "O") {
-         box.innerHTML=`
+    }
+    else if (currentPlayer === "O") {
+        box.innerHTML=`
         <div data-cell-index = ${i}  id="box" onclick="pressBox(this)">
         <img src="/src/icons/O.png" alt="X" style="width:95%; height:95%;">
         </div>
         `
         currentPlayer="X"
-     }
+        
+            console.log(localStorage.getItem("k"));
+            console.log(localStorage.getItem("GridNumber"));
+            
+    }
+    
+    
 
-     
 
 
 }
